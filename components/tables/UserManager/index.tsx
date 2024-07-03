654df7d61ec825e17d6ab7502232ff8b80baa1ee
tinict <tinnguyensp.ict@gmail.com>
2024-07-03 09:59:54 +0700

    [layout]: Layout show list user

---
+++ components/tables/UserManager/index.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import React from "react";
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    User,
+    Chip,
+    Tooltip,
+    getKeyValue
+} from "@nextui-org/react";
+import { EditIcon } from "../../icons/EditIcon";
+import { EyeIcon } from "../../icons/EyeIcon";
+import { columns, users } from "./data";
+import { genderMap } from "@/components/constants";
+
+const statusColorMap: any = {
+    active: "success",
+    paused: "danger",
+    vacation: "warning",
+};
+
+export default function UserManager() {
+    const renderCell = React.useCallback((user: any, columnKey: any) => {
+        const cellValue = user[columnKey];
+
+        switch (columnKey) {
+            case "name":
+                return (
+                    <User
+                        avatarProps={{ radius: "lg", src: user.avatar }}
+                        description={user.email}
+                        name={cellValue}
+                    >
+                        {user.email}
+                    </User>
+                );
+            case "status":
+                return (
+                    <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+                        {cellValue}
+                    </Chip>
+                );
+            case "dob":
+                return (
+                    <Chip className="capitalize" color={statusColorMap[user.dob]} size="sm" variant="flat">
+                        {cellValue}
+                    </Chip>
+                );
+            case "gender":
+                return genderMap[cellValue];
+            case "description":
+                return cellValue;
+            case "actions":
+                return (
+                    <div className="relative flex items-center gap-2">
+                        <Tooltip content="Details">
+                            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                <EyeIcon />
+                            </span>
+                        </Tooltip>
+                        <Tooltip content="Edit user">
+                            <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                <EditIcon />
+                            </span>
+                        </Tooltip>
+                    </div>
+                );
+            default:
+                return cellValue;
+        }
+    }, []);
+
+    return (
+        <Table aria-label="Example table with custom cells">
+            <TableHeader columns={columns}>
+                {(column) => (
+                    <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+                        {column.name}
+                    </TableColumn>
+                )}
+            </TableHeader>
+            <TableBody items={users}>
+                {(item) => (
+                    <TableRow key={item.id}>
+                        {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
+    );
+}
