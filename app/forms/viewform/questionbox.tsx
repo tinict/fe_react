0a5c275cd307357d776088d5bda9450463be207f
tinict <tinnguyensp.ict@gmail.com>
2024-07-28 11:47:48 +0700

    [ui_quiz]: Implement integrate api and mockup data form

--- app/forms/viewform/questionbox.tsx
+++ app/forms/viewform/questionbox.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Radio, RadioGroup } from '@nextui-org/react';
 
-const QuestionBox = () => {
+const QuestionBox = ({ ...props }) => {
+    const { questions } = props;
+
     return (
-        <div className="bg-white p-6 rounded-lg shadow-[rgba(0,0,0,0.05)_0px_0px_0px_1px,rgb(209,213,219)_0px_0px_0px_1px_inset] w-full mb-[16px]">
+        <div
+            className="bg-white p-6 rounded-lg shadow-[rgba(0,0,0,0.05)_0px_0px_0px_1px,rgb(209,213,219)_0px_0px_0px_1px_inset] w-full mb-[16px]"
+        >
             <fieldset className="space-y-4">
                 <legend
                     className="text-[16px] text-[rgb(32,33,36)] font-['docs-Roboto','Helvetica','Arial','sans-serif']"
                 >
-                    Kích thước áo
+                    {questions.name}
                 </legend>
                 <RadioGroup
                     orientation="vertical"
@@ -17,11 +21,13 @@ const QuestionBox = () => {
                     defaultValue="M"
                     className="space-y-2"
                 >
-                    <Radio value="XS">XS</Radio>
-                    <Radio value="S">S</Radio>
-                    <Radio value="M">M</Radio>
-                    <Radio value="L">L</Radio>
-                    <Radio value="XL">XL</Radio>
+                    {(
+                        questions.answers.map((item: any, index: number) => {
+                            return (
+                                <Radio value={item.id} key={index}>{item.value}</Radio>
+                            )
+                        })
+                    )}
                 </RadioGroup>
             </fieldset>
         </div>
