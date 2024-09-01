4424e14fa1b44ce06745307d47f00eb7b04ee258
tinict <tinnguyensp.ict@gmail.com>
2024-09-01 14:59:37 +0700

    [ui-forms]: Implement ui custome config question of forms

--- app/forms/q/[slug]/edit/layout.tsx
+++ app/forms/q/[slug]/edit/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <section className="bg-[rgb(255,255,255)] flex flex-col justify-center w-full">
         <HeadQuiz />
-        <section className="flex justify-center">{children}</section>
+        <section className="grid grid-cols-12">{children}</section>
       </section>
     </>
   );
